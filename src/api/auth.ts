1a1191125f5be58671b19fcbe9490006f28d68c9
juan esteban cubillos <juanesgym2018@gmail.com>
2026-01-24 16:20:44 -0500

    feature: acomodando la conexion con el api

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -24,7 +24,7 @@ import {
 import { format } from 'date-fns';
 axios.defaults.withCredentials = true;
 
-const backendUrl = 'http://localhost:3000';
+const backendUrl = 'backend-canchas-production.up.railway.app'//'http://localhost:3000';
 
 interface GetCourtsResponse {
   success: boolean;
