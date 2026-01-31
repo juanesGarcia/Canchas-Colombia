47d4c37c0f8f6c664440d969c79ebcbb0205d20f
juan esteban cubillos <juanesgym2018@gmail.com>
2026-01-31 15:17:52 -0500

    feature: dejando baclkend de auth

--- src/api/auth.ts
+++ src/api/auth.ts
@@ -24,7 +24,7 @@ import {
 import { format } from 'date-fns';
 axios.defaults.withCredentials = true;
 
-const backendUrl = "http://localhost:8080";
+const backendUrl = "https://backend-canchas-production.up.railway.app";//"http://localhost:8080";
 
 interface GetCourtsResponse {
   success: boolean;
