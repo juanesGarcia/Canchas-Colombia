0a2b7485061fc62186bff6e14d3d61a59bd63c08
juan esteban cubillos <juanesgym2018@gmail.com>
2026-02-01 12:10:27 -0500

    feature: agregando la cuidad de santa marta

--- src/pages/Fields/Fields.tsx
+++ src/pages/Fields/Fields.tsx
@@ -46,6 +46,7 @@ export const Fields: React.FC = () => {
     { value: "Cali", label: "Cali" },
     { value: "Barranquilla", label: "Barranquilla" },
     { value: "Cartagena", label: "Cartagena" },
+     { value: "Santa Marta", label: "Santa Marta" }
   ];
   const priceRanges = [
     { value: "all", label: "Todos los precios" },
