3c04b67af10e37cec36bc35f2f58b272c98accdc
AugustoOspino <augustoosrios@gmail.com>
2026-04-28 11:37:10 -0500

    Feature: actualizacion de mapa

--- src/pages/Contact/Contact.tsx
+++ src/pages/Contact/Contact.tsx
@@ -148,14 +148,14 @@ Quedo atento/a a tu respuesta.
             {/* Map placeholder */}
             <div className="bg-gray-200 dark:bg-gray-700 rounded-lg h-64">
               <div className="h-full w-full">
-               <iframe
+                <iframe
                   src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d3913.4586448827804!2d-74.17897293948668!3d11.227632801239293!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1ses-419!2sco!4v1777393009441!5m2!1ses-419!2sco"
                   className="w-full h-64 rounded-lg"
                   style={{ border: 0 }}
                   allowFullScreen
                   loading="lazy"
                   referrerPolicy="no-referrer-when-downgrade"
-                 />
+                  />
               </div>
             </div>
           </div>
